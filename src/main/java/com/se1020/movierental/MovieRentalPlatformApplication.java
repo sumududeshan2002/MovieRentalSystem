e9468d80497533752306ae9c9f397ed042254424
sumududeshan2002 <sumududeshandias@gmail.com>
2026-05-19 19:29:05 +0530

    Initial project setup - Spring Boot War project

---
+++ src/main/java/com/se1020/movierental/MovieRentalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.se1020.movierental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentalPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRentalPlatformApplication.class, args);
+    }
+
+}
