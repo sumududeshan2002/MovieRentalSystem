0f2aa041562bad4497eea27ca0846a513dbea239
sumududeshan2002 <sumududeshandias@gmail.com>
2026-05-20 02:14:46 +0530

    fix: correct package structure to com.se1020.movierental

---
+++ src/test/java/com/se1020/movierental/MovierentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se1020.movierental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
